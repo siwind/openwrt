ae3621e37d7eac839c505e39ed8493d3d0bc80f4
siwind <yinqingwang@gmail.com>
2020-04-28 16:18:01 +0800

    zte e8820v2 lzma loader

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1027,12 +1027,12 @@ endef
 TARGET_DEVICES += zio_freezio
 
 define Device/zte_e8820v2
-  SOC := mt7621
+  $(Device/uimage-lzma-loader)
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := E8820V2
-  DEVICE_PACKAGES := \
-	kmod-mt7603 kmod-mt76x2 kmod-usb3 kmod-usb-ledtrig-usbport wpad-basic
+  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb3 \
+	  kmod-usb-ledtrig-usbport wpad-basic
 endef
 TARGET_DEVICES += zte_e8820v2
 
