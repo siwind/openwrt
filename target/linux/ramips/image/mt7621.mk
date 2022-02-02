aaf5b1012432fa00361046c8e655153bc23e5316
siwind <yinqingwang@163.com>
2022-02-02 13:43:58 +0800

    ramips: mediatek mt7621: zte-e8820v2 fix compile issue with wpad-basic

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1892,7 +1892,7 @@ define Device/zte_e8820v2
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := E8820V2
   DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb2 \
-	  kmod-usb-ledtrig-usbport wpad-basic
+	  kmod-usb-ledtrig-usbport
 endef
 TARGET_DEVICES += zte_e8820v2
 
